e20be757b483addad6ae2ddc01ea997cc627a09f
itsvmgovind <itsvmgovind@gmail.com>
2021-02-28 00:15:27 +0530

    Add Executed Log to track

--- src/main/java/HelloJenkins.java
+++ src/main/java/HelloJenkins.java
@@ -1,7 +1,9 @@
+import java.time.LocalDateTime;
 
 public class HelloJenkins {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Jenkins World");
+		System.out.println("Build Executed @"+ LocalDateTime.now());
 	}
 }
