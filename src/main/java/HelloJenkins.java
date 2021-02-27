8f6ebc1b15c46e7599aefe3b302cf593e299d753
itsvmgovind <53965183+itsvmgovind@users.noreply.github.com>
2021-02-28 00:46:11 +0530

    Update HelloJenkins.java

--- src/main/java/HelloJenkins.java
+++ src/main/java/HelloJenkins.java
@@ -5,7 +5,5 @@ public class HelloJenkins {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Jenkins World");
 		System.out.println("Build Executed @"+ LocalDateTime.now());
-		System.out.println("Welcome to Jenkins World");
-		System.out.println("Build Executed @"+ LocalDateTime.now());
 	}
 }
